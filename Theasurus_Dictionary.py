4125c03f222305967a4bf2dce412727040a11f7e
madhav1698 <madhavoffic@gmail.com>
2021-05-01 18:24:31 -0600

    added comment abt similarity ratio

--- Theasurus_Dictionary.py
+++ Theasurus_Dictionary.py
@@ -10,7 +10,7 @@ def translate(w):
         return data[w.title()]
     elif w.upper() in data:
         return data[w.upper()]
-    elif len(get_close_matches(w,data.keys()))>0:
+    elif len(get_close_matches(w,data.keys()))>0: #gets closest matches by checking its similarity ratio
         yn=input("Did you mean %s instead? Enter y if yes,or n if no :" %get_close_matches(w,data.keys())[0])
         if yn=="y":
             return data[get_close_matches(w,data.keys())[0]]
