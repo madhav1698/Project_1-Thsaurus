cf951d0b4e16b0c98afdfd8c3b1a6337201d87bc
madhav1698 <madhavoffic@gmail.com>
2021-05-01 18:19:31 -0600

    init

---
+++ Theasurus_Dictionary.py
@@ -0,0 +1,31 @@
+import json
+import difflib
+from difflib import get_close_matches
+data=json.load(open("data.json"))
+def translate(w):
+    w=w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w,data.keys()))>0:
+        yn=input("Did you mean %s instead? Enter y if yes,or n if no :" %get_close_matches(w,data.keys())[0])
+        if yn=="y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn=="n":
+            return "This word does not exist. Please check again"
+        else:
+            return "Sorry. We didn't understand your query"
+    else:
+        return "This word does not exist. Please check again"
+
+word=input("Enter word:")
+output=translate(word)
+
+if type(output)==list:
+    for i in output:
+        print(i)
+else:
+    print(output)
